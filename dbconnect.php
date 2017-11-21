1930c87bef385e13bf99491451ab8a2cf50ec4c8
Ritabhash <31769849+Ritabhash@users.noreply.github.com>
2017-11-21 20:29:41 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,18 +1,4 @@
-<!--?php
 
-$db_host = "localhost";
-$db_name = "register";
-$db_user = "root";
-$db_pass = "toor";
-
-
-$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
---->
 
 <?php
 
